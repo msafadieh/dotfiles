6375b3bd0f4585e53c5240372d556fd69c796c8f
Mohamad Safadieh <me@msafadieh.com>
2019-09-10 00:09:59 -0400

    switch to bitwarden

--- .scripts/get-gpg.sh
+++ .scripts/get-gpg.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
-[[ $(lpass status) == "Not logged in." ]] && lpass login me@msafadieh.com
-lpass show 7973861828950534859 --password --clip && notify-send "Password copied!" || notify-send "Error!"
+bw sync
+[[ $? -ne 0 ]] && export BW_SESSION=$(zenity --password | bw unlock --raw)
+bw get password PGP | xclip -selection clipboard && notify-send "Password copied!" || notify-send "Error!"

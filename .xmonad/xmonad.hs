99c7b3fb5ad6ce79ca7d2563ec8c695944cd12b9
Mohamad Safadieh <me@msafadieh.com>
2019-11-27 08:42:06 -0500

    remove redundant alacritty option and touchpad config from xmonad

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -30,7 +30,6 @@ main = do
         bmapplet <- spawnPipe "blueman-applet"
         picom <- spawnPipe "picom"
         stalonetray <- spawnPipe "stalonetray"
-        xinput <- spawnPipe "xinput set-button-map 'ELAN1300:00 04F3:3087 Touchpad' 1 1 3 4 5 6 7 8 9 10"
         xflux <- spawnPipe "pidof xflux || xflux -z 12604"
         xautolock <- spawnPipe "xautolock -time 15 -locker 'systemctl suspend' -detectsleep -corners '0-00'"
         xmproc <- spawnPipe "xmobar $HOME/.xmonad/xmobar"
